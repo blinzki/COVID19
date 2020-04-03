77f489e30543bf5d2d4d115514ca6ea4a687791b
blinzki <ing.pabloeromero@gmail.com>
2020-04-03 03:35:17 -0300

    Clean code

--- SEIR-model/SEIR-model.py
+++ SEIR-model/SEIR-model.py
@@ -18,13 +18,11 @@ beta = 1.42
 gamma = 1./5
 sigma = 1./7
 
-# Initial number of infected and recovered individuals, I0 and R0.
+# Initial conditions.
 I0, R0, E0 = 1, 0, 0
-
-# Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0 - E0
 
-# A grid of time points (in days)
+# Grid of time 
 t = np.linspace(0, x_axis, x_axis)
 
 # The SEIR model differential equations.
@@ -46,7 +44,7 @@ x = [1, 1, 1, 1, 1, 2, 2, 2, 2, 8, 8, 11, 13, 21, 24, 33, 48, 55, 62, 65]
 ret = odeint(deriv, y0, t, args=(N, beta, gamma, sigma))
 S, E, I, R = ret.T
 
-# Plot the data on three separate curves for S(t), I(t) and R(t)
+# Plot the data curves: S(t), E(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111,  axisbelow=True)
 ax.plot(t, S, 'b', alpha=0.5, lw=1, label='Susceptible')
@@ -54,7 +52,7 @@ ax.plot(t, E, 'y', alpha=0.5, lw=1, label='Exposed')
 ax.plot(t, I, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, R, 'g', alpha=0.5, lw=1, label='Recovered with immunity')
 ax.plot(x, 'o', label='Confirmed cases')
-ax.set_xlabel('Time /days')
+ax.set_xlabel('Time/days')
 ax.set_ylabel('Number')
 ax.set_ylim(0,y_axis)
 ax.yaxis.set_tick_params(length=0)
