aabb03ac02a3e54120864e8d45d9e625e217e73e
blinzki <ing.pabloeromero@gmail.com>
2020-04-16 00:45:35 -0300

    test

--- test_SEIR-model.py
+++ test_SEIR-model.py
@@ -9,29 +9,6 @@ import math
 # ing.pabloeromero@gmail.com
 # 03/2020
 
-## Chart config
-
-# General 
-y_axis = 1000000
-x_axis = 180 
-
-# Zoom
-y_axis = 100 
-x_axis = 35 
-
-# Model parameters
-N = 992323
-beta = 1.38  
-gamma = 1./5
-sigma = 1./7
-b1 = beta
-# Initial conditions.
-I0, R0, E0 = 1, 0, 0
-S0 = N - I0 - R0 - E0
-
-# Grid of time
-t = np.linspace(0, x_axis, x_axis)
-
 # The SEIR model differential equations.
 def deriv(y, t, N, beta, gamma, sigma):
     S, E, I, R = y
@@ -41,74 +18,101 @@ def deriv(y, t, N, beta, gamma, sigma):
     dRdt = gamma * I
     return dSdt, dEdt, dIdt, dRdt
 
-# Initial conditions vector
-y0 = S0, E0, I0, R0
+def predict(chart_number):
 
-# Real data Gran Rosario 
-#x = [1, 1, 1, 1, 1, 2, 2, 2, 2, 8, 8, 11, 13, 21, 24, 33, 48, 55, 62, 65, 72,,,,,,,93. 94, 94]
+   # Model parameters
+   N = 992323
+   beta = 1.38
+   gamma = 1./5
+   sigma = 1./7
+   b1 = beta
 
-# Real data Municipo de Rosario
-x = [0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 8, 8, 12, 20, 23, 33, 44, 51, 56, 58, 65, 68, 73, 76, 77, 79, 79, 80, 81, 84, 85, 85]
+   # Initial conditions.
+   I0, R0, E0 = 1, 0, 0
+   S0 = N - I0 - R0 - E0 
 
-# Integrate the SEIR equations over period 1  
+   y_axis = 1000000
+   x_axis = 180 
 
-ret = odeint(deriv, y0, t, args=(N, beta, gamma, sigma))
-S1, E1, I1, R1 = ret.T
+   if chart_number == 1:
+      y_axis = 100 
+      x_axis = 35 
+    
+   # Grid of time
+   t = np.linspace(0, x_axis, x_axis)
+ 
+   # Initial conditions vector
+   y0 = S0, E0, I0, R0
 
-# Finding beta at the last point
+   # Real data Gran Rosario 
+   #x = [1, 1, 1, 1, 1, 2, 2, 2, 2, 8, 8, 11, 13, 21, 24, 33, 48, 55, 62, 65, 72,,,,,,,93. 94, 94]
+ 
+   # Real data Municipo de Rosario
+   x = [0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 8, 8, 12, 20, 23, 33, 44, 51, 56, 58, 65, 68, 73, 76, 77, 79, 79, 80, 81, 84, 85, 85]
 
-# beta from 0.8 to 1.2 + delta in 20 steps
-step = 20000 
-delta = 3 
-beta = 0.4
+   # Integrate the SEIR equations over period 1  
 
-l = len(x)
-for i in range(step):
-   b =  beta + delta  * (i + 1) / step
-   ret = odeint(deriv, y0, t, args=(N, b, gamma, sigma))
-   S2, E2, I2, R2 = ret.T
-   #if x[l-1] > I2[l]:
-   print(l-1, I2[l-1], x[l-1], sep='\t')    	
-   if I2[l-1] >= x[l-1]:
-      beta = b
-      break 
-b2 = beta
+   ret = odeint(deriv, y0, t, args=(N, beta, gamma, sigma))
+   S1, E1, I1, R1 = ret.T
 
-# Print predictions
-for i in range(x_axis):
-    initd = '2020-03-14'
-    date = datetime.strptime(initd, "%Y-%m-%d")
-    d = date + timedelta(days=i)
-    #print(i, str(d.strftime("%Y-%m-%d")), math.floor(I1[i]), sep='\t')
+   # Finding beta at the last point
 
-# R0 interpolation
-ro1=str(round(b1/gamma, 2))
-ro2=str(round(b2/gamma, 2))
+   # beta from 0.8 to 1.2 + delta in 20 steps
+   step = 20000 
+   delta = 3 
+   beta = 0.4
 
-# Plot the data curves: S(t), E(t), I(t) and R(t)
-fig = plt.figure(facecolor='w')
-ax = fig.add_subplot(111,  axisbelow=True)
-ax.set_title('Rosario SEIR Model COVID-19')
-ax.plot( S2, 'b', alpha=0.5, lw=1, label='Susceptible')
-ax.plot( E2, 'y', alpha=0.5, lw=1, label='Exposed')
-ax.plot( I1, 'r', alpha=0.5, lw=2, label='Infected without containment')
-ax.plot( I2, 'r', alpha=0.5, lw=2, label='Infected with containment')
-ax.plot( R2, 'g', alpha=0.5, lw=1, label='Recovered with immunity')
-ax.plot( x, '-', label='Confirmed case')
-ax.plot( I2, 'o', color='red',label='Free containment')
+   l = len(x)
+   for i in range(step):
+      b =  beta + delta  * (i + 1) / step
+      ret = odeint(deriv, y0, t, args=(N, b, gamma, sigma))
+      S2, E2, I2, R2 = ret.T
+      #if x[l-1] > I2[l]:
+      print(l-1, I2[l-1], x[l-1], sep='\t')    	
+      if I2[l-1] >= x[l-1]:
+         beta = b
+         break 
+   b2 = beta
 
-ax.annotate("$R1_{0}$="+ ro1, xy=(0.9,0.99),xycoords='axes fraction', fontsize=10)
-ax.annotate("$R2_{0}$="+ ro2, xy=(0.9,0.9),xycoords='axes fraction', fontsize=10)
-ax.set_xlabel('Time/days')
-ax.set_ylabel('Number')
-ax.set_ylim(0,y_axis)
-ax.yaxis.set_tick_params(length=0)
-ax.xaxis.set_tick_params(length=1)
-ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-legend = ax.legend()
-legend.get_frame().set_alpha(0.5)
+   # Print predictions
+   for i in range(x_axis):
+      initd = '2020-03-14'
+      date = datetime.strptime(initd, "%Y-%m-%d")
+      d = date + timedelta(days=i)
+      #print(i, str(d.strftime("%Y-%m-%d")), math.floor(I1[i]), sep='\t')
 
-for spine in ('top', 'right', 'bottom', 'left'):
-    ax.spines[spine].set_visible(False)
+   # R0 interpolation
+   ro1=str(round(b1/gamma, 2))
+   ro2=str(round(b2/gamma, 2))
 
-plt.show()
+   # Plot the data curves: S(t), E(t), I(t) and R(t)
+   fig = plt.figure(facecolor='w')
+   ax = fig.add_subplot(111,  axisbelow=True)
+   ax.set_title('Rosario SEIR Model COVID-19')
+   ax.plot( S2, 'b', alpha=0.5, lw=1, label='Susceptible')
+   ax.plot( E2, 'y', alpha=0.5, lw=1, label='Exposed')
+   ax.plot( I1, 'r', alpha=0.5, lw=2, label='Infected without containment')
+   ax.plot( I2, 'r', alpha=0.5, lw=2, label='Infected with containment')
+   ax.plot( R2, 'g', alpha=0.5, lw=1, label='Recovered with immunity')
+   ax.plot( x, '-', label='Confirmed case')
+   if chart_number == 1: ax.plot( I2, 'o', color='red',label='Free containment')
+
+   ax.annotate("$R1_{0}$="+ ro1, xy=(0.9,0.99),xycoords='axes fraction', fontsize=10)
+   ax.annotate("$R2_{0}$="+ ro2, xy=(0.9,0.9),xycoords='axes fraction', fontsize=10)
+   ax.set_xlabel('Time/days')
+   ax.set_ylabel('Number')
+   ax.set_ylim(0,y_axis)
+   ax.yaxis.set_tick_params(length=0)
+   ax.xaxis.set_tick_params(length=1)
+   ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+   legend = ax.legend()
+   legend.get_frame().set_alpha(0.5)
+
+   for spine in ('top', 'right', 'bottom', 'left'):
+       ax.spines[spine].set_visible(False)
+
+   plt.show()
+ 
+def test_interpolation():
+   __test__ = False
+   predict(2)
