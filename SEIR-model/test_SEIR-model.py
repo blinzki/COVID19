729c3b159c72afde639c407e1f3bbc1a5d5f5803
blinzki <ing.pabloeromero@gmail.com>
2020-04-30 21:41:52 -0300

    Upadate data

--- SEIR-model/test_SEIR-model.py
+++ SEIR-model/test_SEIR-model.py
@@ -158,4 +158,4 @@ def predict(chart_number, last_day , last_infected, last_exposed):
 
 def test_interpolation():
    __test__ = False
-   predict(1, "2020-04-27", 98, 104)
+   predict(1, "2020-04-28", 98, 68)
