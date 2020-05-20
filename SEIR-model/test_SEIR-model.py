61f053577fb00a186a247792f3780fc630fb1b90
blinzki <ing.pabloeromero@gmail.com>
2020-05-19 22:04:14 -0300

    Update data

--- SEIR-model/test_SEIR-model.py
+++ SEIR-model/test_SEIR-model.py
@@ -158,4 +158,4 @@ def predict(chart_number, last_day , last_infected, last_exposed):
 
 def test_interpolation():
    __test__ = False
-   predict(1, "2020-05-17", 99, 126)
+   predict(1, "2020-05-18", 103, 72)
